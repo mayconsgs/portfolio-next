40005909ae623a4400d3946c00ea8d8e0dc3f2d6
Maycon Santos <maycon.s.santos44@gmail.com>
2021-03-05 17:22:43 -0300

    Save para formatar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="pt-br">
         <Head>
           <link rel="icon" id="icon-webSite" href="favicon.svg" />
         </Head>
